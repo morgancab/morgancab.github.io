2513eef1d6791a2f79ece75d9d2ec5c4fa4a4c10
Morgan Cabedoche <morgan2509@live.fr>
2023-04-15 19:22:54 +0200

    Feedback change

--- js/main.js
+++ js/main.js
@@ -108,6 +108,7 @@
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
+        autoplayTimeout:7000 ,
         dots: true,
         loop: true,
         items: 1
